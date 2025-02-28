212737edf951cfc44d0c5039a95cc9871e721556
Michael Shumakov <michael.shumakov@gmail.com>
2025-02-28 10:04:20 -0700

    feat: pass style attrs to inputs

--- components/utils/ControlsMenu.vue
+++ components/utils/ControlsMenu.vue
@@ -9,37 +9,54 @@ const typeScaleOptions = [
   { label: 'Perfect Fifth', value: 1.5 },
   { label: 'Golden Ratio', value: 1.618 },
 ]
+
+const styles = useState<TStyles>('styles')
 </script>
 <template>
-  <div class="controls-content flex flex-col flex-gap-base">
+  <div v-if="styles" class="controls-content flex flex-col flex-gap-base">
     <div>
       <menu
         aria-labelledby="menu-layout-heading"
         class="controls-content__menu"
       >
         <li>
-          <ElementsInputCheckbox type="checkbox" id="dark-mode"
+          <ElementsInputCheckbox
+            type="checkbox"
+            id="dark-mode"
+            v-model="styles.isDark"
             >dark mode</ElementsInputCheckbox
           >
         </li>
         <li>
-          <ElementsInputCheckbox type="checkbox" id="toggle-overlay"
+          <ElementsInputCheckbox
+            type="checkbox"
+            id="toggle-overlay"
+            v-model="styles.showOverlay"
             >show overlay</ElementsInputCheckbox
           >
         </li>
         <li>
-          <ElementsInputNumber id="base-grid-cols"
+          <ElementsInputNumber id="base-grid-cols" v-model="styles.gridCols"
             >grid columns</ElementsInputNumber
           >
         </li>
         <li>
-          <ElementsInputNumber id="base-text-cols"
-            >text columns</ElementsInputNumber
+          <ElementsInputNumber id="base-text-cols" v-model="styles.textColWidth"
+            >text column width (ch)</ElementsInputNumber
           >
         </li>
         <li>
-          <ElementsInputNumber id="font-size"
-            >base font size</ElementsInputNumber
+          <ElementsInputNumber
+            id="min-font-size"
+            v-model="styles.minBaseFontSize"
+            >min base font size</ElementsInputNumber
+          >
+        </li>
+        <li>
+          <ElementsInputNumber
+            id="max-font-size"
+            v-model="styles.maxBaseFontSize"
+            >max base font size</ElementsInputNumber
           >
         </li>
         <li>
@@ -47,6 +64,7 @@ const typeScaleOptions = [
             :options="typeScaleOptions"
             placeholder="Enter value"
             id="type-scale"
+            v-model="styles.typeScale"
           >
             <template #label>type scale</template>
             <template #optLabel="{ option }">
@@ -59,15 +77,15 @@ const typeScaleOptions = [
           </ElementsInputSelect>
         </li>
         <li>
-          <ElementsInputNumber id="line-height"
+          <ElementsInputNumber id="line-height" v-model="styles.lineHeightBase"
             >line height</ElementsInputNumber
           >
         </li>
-        <li>
+        <!-- <li>
           <ElementsInputCheckbox type="checkbox" id="baseline-align"
             >align to baseline</ElementsInputCheckbox
           >
-        </li>
+        </li> -->
       </menu>
     </div>
   </div>
