2c08f144502bc28d0fd0e109303ffe5a0496db7d
Michael Shumakov <michael.shumakov@gmail.com>
2025-02-28 10:03:38 -0700

    feat: add types export

---
+++ utils/types.ts
@@ -0,0 +1,14 @@
+export type TStyles = {
+  gridCols: number
+  isDark: boolean
+  showOverlay: boolean
+  textCols: number
+  textColWidth: number
+  lineHeightBase: number
+  typeScale: number
+  minBaseFontSize: number
+  maxBaseFontSize: number
+  alignToBaseline: boolean
+}
+
+export type TVars = Record<string, number>
