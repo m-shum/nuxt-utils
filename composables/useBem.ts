8679d38a3227b21658d960501b7b57dcb6b262f9
Michael Shumakov <62724405+m-shum@users.noreply.github.com>
2025-03-14 17:13:47 -0700

    feat: add types

--- composables/useBem.ts
+++ composables/useBem.ts
@@ -1,36 +1,35 @@
 /*
-base: 'base'
-basic class: 'base__el'
-modifier: 'base__el base__el--modifier'
-conditional modifier: 'base__el base__el--modifier'
-multiple modifiers: 'base__el base__el--modifier1 base__el--modifier2'
 
-const bem = useBem('base') --> output: fn bem()
-basic class = bem('element') --> bem is fed 1 arg, the element
-basic modifier = bem('element', 'modifier') --> output = 'base__element base__element--modifier'
-multiple modifiers = bem('element', ['modifier1', modifier2']) --> output = 'base__element base__element--modifier1 base__element--modifier2'
-conditional modifier = bem('element', 'modifier' && condition)
-multipple conditional modifiers = bem('element', ['modifier1' && condition, 'modifier2' && condition])
+USAGE
+
+bem('element') -> `baseClass__element`
+bem('element', 'modifier') -> `baseClass__element baseClass__element--modifier`
+bem('element', ['modifier1', 'modifier2']) -> `baseClass__element baseClass__element--modifier1 baseClass__element--modifier2`
+bem('element', 'modifier' && booleanCondition) -> if condition = true: `baseClass__element baseClass__element--modifier` else `baseClass__element`
+bem('element', ['modifier1' && booleanCondition, 'modifier2']) -> if condition = true: `baseClass__element baseClass__element--modifier1 baseClass__element--modifier2` else `baseClass__element baseClass__element--modifier2`
+.... so on
 */
 
+type TMod = boolean | undefined | null | string
+
+type TArgs = [string, TMod | TMod[]] | [string]
+
 export const useBem = (base: string) => {
-  const constructClasses = (...args: any) => {
-    const [el, modifiers] = args
+  const mkCl = (...args: TArgs) => {
+    const [el, mod] = args
 
-    const baseClass = `${base}__${el}`
+    const cl = `${base}__${el}`
 
-    if (!modifiers) return baseClass
-    if (isNonEmptyString(modifiers))
-      return `${baseClass} ${baseClass}--${modifiers}`
+    if (!mod) return cl
+    if (isNonEmptyString(mod)) return `${cl} ${cl}--${mod}`
 
-    return modifiers.reduce(
-      (className: string, modifier: string, idx: number) => {
-        className = `${className} ${baseClass}--${modifier}`
-        return className
-      },
-      baseClass
-    )
+    return (mod as TMod[]).reduce((acc: string, mod: TMod) => {
+      if (isNonEmptyString(mod)) {
+        acc = `${acc} ${acc}--${mod}`
+      }
+      return acc
+    }, cl)
   }
 
-  return constructClasses
+  return mkCl
 }
